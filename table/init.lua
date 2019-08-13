89b42f3ec7e34428cd8c946299e00922567805f5
Grandolf <Grandolf2000@gmail.com>
2019-08-13 09:46:43 -0400

    Add files via upload

---
+++ table/init.lua
@@ -0,0 +1,8 @@
+minetest.register_node("table:table", {
+	description = "table",
+	tiles = {"table_table.png"},
+	mesh = "table.obj",
+	groups = {choppy=1},
+	sounds = default.node_sound_wood_defaults(),
+})
+
